7e8372c3d5c14dfe4fa177a9e203e4e0dc5ec720
azzali198 <med_azzali@yahoo.fr>
2025-06-02 23:24:01 +0200

    fixing bug

--- EducationGameProject/Back/PhysicsGame.DAL/obj/Debug/net8.0/PhysicsGame.DAL.AssemblyInfo.cs
+++ EducationGameProject/Back/PhysicsGame.DAL/obj/Debug/net8.0/PhysicsGame.DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhysicsGame.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c091ecf835274e72fc16fc08101e86aefc3c6722")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2da7d411f5fa9957601eca7d51b291dfe9d9842d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhysicsGame.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhysicsGame.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
